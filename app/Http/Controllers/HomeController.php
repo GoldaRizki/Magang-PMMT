d0e94a8b52cfc78ea15cf12ef3d4df01bb85ac56
goldarizki <goldarizki@gmail.com>
2023-08-08 08:14:07 +0700

    Update fitur pengecekan sebelum semua halaman.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+   /* public function _construct(){
+        parent::__construct();  
+    }*/
+
+    public function index(){
+
+        return view('coba');
+      
+    }
+
+
+   
+}
